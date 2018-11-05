db805bc5777656765f6a29c0f14138cb516aea42
Egor Ivkov <e.o.ivkov@gmail.com>
2018-11-05 16:06:51 +0300

    Board interface for better server integration

---
+++ Chess Game Example Project/ChessGame/Assets/ChessGameScripts/ChessLogicAndGameplay/IBoard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace AWSSDK.Examples.ChessGame
+{
+    public interface IBoard
+    {
+        ChessData.ChessPieceColor GetTurnColor();
+
+        ChessData.ChessMove GetPreviousMove();
+
+        IEnumerable<ChessData.ChessMove> GetPossibleMoves(ChessData.Coordinate coordinate);
+
+        bool TryApplyMove(ChessData.ChessMove newMove);
+
+        string GetForsythEdwardsNotation();
+
+        ChessData.ChessPiece GetPieceAtCoordinate(ChessData.Coordinate coordinate);
+    }
+}
