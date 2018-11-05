d7bf563e83b4e6674eb179c6b9a2af523299f7c5
Egor Ivkov <e.o.ivkov@gmail.com>
2018-11-05 18:50:22 +0300

    try apply move request

--- Chess Game Example Project/ChessGame/Assets/ChessGameScripts/ChessLogicAndGameplay/Board.cs
+++ Chess Game Example Project/ChessGame/Assets/ChessGameScripts/ChessLogicAndGameplay/Board.cs
@@ -67,7 +67,20 @@ namespace AWSSDK.Examples.ChessGame
 
         public bool TryApplyMove(ChessData.ChessMove newMove)
         {
-            throw new System.NotImplementedException();
+            var request = new Dictionary<string, string>();
+            request.Add("from_row", newMove.From.Row.ToString());
+            request.Add("to_row", newMove.To.Row.ToString());
+            request.Add("from_column", newMove.From.Column.ToString());
+            request.Add("to_column", newMove.To.Column.ToString());
+            request.Add("chess_piece_type", ((int)newMove.PieceType).ToString());
+            request.Add("is_capture", newMove.IsCapture.ToString());
+            request.Add("is_promotion_to_queen", newMove.IsPromotionToQueen.ToString());
+            request.Add("draw_offer_extended", newMove.DrawOfferExtended.ToString());
+            request.Add("is_check", newMove.IsCheck.ToString());
+            request.Add("is_check_mate", newMove.IsCheckMate.ToString());
+            request.Add("kingsideCastle", newMove.KingsideCastle.ToString());
+            request.Add("queenside_castle", newMove.QueensideCastle.ToString());
+            return bool.Parse(GetResponse("try_apply_move", request));
         }
 
         public string GetForsythEdwardsNotation()
