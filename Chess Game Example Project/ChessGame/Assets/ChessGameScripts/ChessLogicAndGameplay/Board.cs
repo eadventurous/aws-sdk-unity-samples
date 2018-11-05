38c00fb304fcdf69dc93117efc4ef0191012e533
Egor Ivkov <e.o.ivkov@gmail.com>
2018-11-05 17:31:01 +0300

    Board Http Get Responses

--- Chess Game Example Project/ChessGame/Assets/ChessGameScripts/ChessLogicAndGameplay/Board.cs
+++ Chess Game Example Project/ChessGame/Assets/ChessGameScripts/ChessLogicAndGameplay/Board.cs
@@ -1,7 +1,77 @@
-﻿namespace AWSSDK.Examples.ChessGame
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace AWSSDK.Examples.ChessGame
 {
-    public class Board
+    public class Board: IBoard
     {
+        private int _id;
         
+        public Board(int matchId)
+        {
+            var requestParams = new Dictionary<string, string>();
+            requestParams.Add("matchId", matchId.ToString());
+            _id = int.Parse(GetResponse(requestParams));
+            
+        }
+
+        private static readonly string GatewayUri = "";
+
+        public string GetResponse(Dictionary<String, String> requestParams)
+        {
+            string additionalUri = "?";
+            foreach (var key in requestParams.Keys)
+            {
+                additionalUri += key + "=" + requestParams[key];
+            }
+            using (UnityWebRequest www = UnityWebRequest.Get(GatewayUri+additionalUri))
+            {
+                www.Send();
+                
+                while(!www.isDone){}
+
+                if (www.isNetworkError || www.isHttpError)
+                {
+                    Debug.Log(www.error);
+                    throw new Exception(www.error);
+                }
+
+                // Show results as text
+                Debug.Log(www.downloadHandler.text);
+                return www.downloadHandler.text;
+            }
+        }
+        
+        public ChessData.ChessPieceColor GetTurnColor()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ChessData.ChessMove GetPreviousMove()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<ChessData.ChessMove> GetPossibleMoves(ChessData.Coordinate coordinate)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool TryApplyMove(ChessData.ChessMove newMove)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string GetForsythEdwardsNotation()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ChessData.ChessPiece GetPieceAtCoordinate(ChessData.Coordinate coordinate)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
